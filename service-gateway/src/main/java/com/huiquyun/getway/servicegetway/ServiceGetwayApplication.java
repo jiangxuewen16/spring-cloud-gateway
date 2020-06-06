6ced992e1360597a18d3d11d1bf28e388fc30358
蒋雪文 <445251692@qq.com>
2020-06-06 16:44:26 +0800

    初始化项目

---
+++ service-gateway/src/main/java/com/huiquyun/getway/servicegetway/ServiceGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.huiquyun.getway.servicegetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGetwayApplication.class, args);
+    }
+
+}
---
+++ service-gateway/src/test/java/com/huiquyun/getway/servicegetway/ServiceGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huiquyun.getway.servicegetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
